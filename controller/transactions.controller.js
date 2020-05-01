9f0e98eda0a2857478272c56579d556e48241d8a
Lâm Tuấn <tupikano1@gmail.com>
2020-05-01 23:36:57 +0700

    fix bai15/1

--- controller/transactions.controller.js
+++ controller/transactions.controller.js
@@ -1,9 +1,12 @@
-const uuid = require('uuid/v4');
+const uuid = require('uuid/v1');
 const db = require('../db.js');
 
 let getAllTransactions= (req, res) => {
+  let trans = db.get("transactions").value()
+  trans.filter(item => item.userId === req.cookies.userId)
   return res.render("transactions/index", {
-    transactions: db.get("transactions").value()
+    transactions: trans,
+    user: db.get("users").find({userId: req.cookies.userId}).value()
   })
 };
 
@@ -11,13 +14,15 @@ let createNewTransactionPage =(req, res) => {
   return res.render("transactions/create", {
     users: db.get('users').value(),
     books: db.get('books').value(),
+    user: db.get("users").find({userId: req.cookies.userId}).value()
   });
 };
 
 let createNewTransaction = (req, res) => {
   db.get('transactions').push({transId: uuid(),bookId: req.body.bookId, userId: req.body.userId }).write();
-  return res.redirect("/transactions/create");  
+  return res.redirect("/transactions");  
 };
+
 let Complete = (req, res) => {
   let transId = req.params.transId;
   let errors = [];
@@ -32,7 +37,8 @@ let Complete = (req, res) => {
     errors.push('Id này không tồn tại');
     return res.render("transactions/", {
       errors: errors,
-      transactions: db.get("transactions").value()
+      transactions: db.get("transactions").value(),
+      user: db.get("users").find({userId: req.cookies.userId}).value()
     })
   }
 };
--- controller/users.controller.js
+++ controller/users.controller.js
@@ -6,27 +6,30 @@ const db = require("../db.js");
 
 let getAllUser = (req, res) => {
   return res.render("users/users.pug", {
-    users: db.get("users").value()
+    users: db.get("users").value(),
+    user: db.get("users").find({userId: req.cookies.userId}).value()
   });
 };
 let createUser = (req, res) => {
-  return res.render("users/create.pug");
+  return res.render("users/create.pug" ,{
+    user: db.get("users").find({userId: req.cookies.userId}).value()
+  });
 }
 let createNewUser = (req, res) => {
   let name = req.body.name;
   let email = req.body.email;
-  db.get("users").push({userId: uuid(), name, email,password: '123123'}).write();
   let errors = [];
   let oldUser = db.get('users').find({email}).value();
   if(oldUser) {
     errors.push('User này đã tồn tại')
     return res.render("users/users", {
       errors: errors,
-      users: db.get("users").value()
+      users: db.get("users").value(),
+      user: db.get("users").find({userId: req.cookies.userId}).value()
     })
   }
-  db.get("users").push({userId: uuid(), name, email, password: '123123'}).write();
-  return res.redirect("/users")
+  db.get("users").push({userId: uuid(), name, email,password: '123123', isAdmin: false}).write();
+    return res.redirect("/users")
 };
 let deleteAnUser =(req, res) => {
   let userId = req.params.userId;
@@ -37,7 +40,8 @@ let deleteAnUser =(req, res) => {
 let updateNameUserPage = (req, res) => {
   let userId = req.params.userId;
   return res.render("users/change-name.pug",{
-    userId
+    userId,
+    user: db.get("users").find({userId: req.cookies.userId}).value()
   })
 };
 let updateNameUser = (req, res) => {
@@ -46,9 +50,7 @@ let updateNameUser = (req, res) => {
   db.get("users").find({userId}).assign({name}).write();
   return res.redirect("/users");
 };
-let login = (req, res) => {
-  res.render("users/login.pug")
-}
+
 module.exports = {
   getAllUser,
   createUser,
@@ -56,5 +58,4 @@ module.exports = {
   deleteAnUser,
   updateNameUser,
   updateNameUserPage,
-  login
 }
