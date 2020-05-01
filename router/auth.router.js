6c35b48c2ba144d4cd090f9c6e0407ad1ef1b92b
Lâm Tuấn <tupikano1@gmail.com>
2020-05-01 15:25:06 +0700

    bai15/1

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const authController = require('../controller/auth.controller');
 
+router.get("/login", authController.login)
 
 
 module.exports = router;
